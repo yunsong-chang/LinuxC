6d0daa0b4d9d4f124005bea49e01f34ab3da827e
u10 <u10@tcl.com>
2017-07-12 12:18:48 +0800

    ch18 volatile: 禁止编译器优化volatile, cache

--- Makefile
+++ Makefile
@@ -3,5 +3,17 @@ SRC = main
 all:
 	gcc $(SRC).c -Wall -g
 
+O:
+	gcc $(SRC).c -Wall -g -O
+
+volatile:
+	gcc $(SRC)_volatile.c -Wall -g -O
+
 clean:
 	-rm *.o *.out 
+
+# 多次读、多次写设备是有意义的
+# 禁止编译器优化: volatile，但若加了-O选项对中间变量还是会用寄存器缓存
+#				  volatile变量：不会省略对变量的每一次内存读写，但有可能数据来自cache而不是内存
+#						程序的指令中只有寄存器、内存地址，没法操控cache, cache对程序是透明的
+# 禁止cache
--- main_volatile.c
+++ main_volatile.c
@@ -1,18 +1,20 @@
 /* artificial device registers */
-unsigned char recv;
-unsigned char send;
+volatile unsigned char recv;
+volatile unsigned char send;
 
 /* memory buffer */
 unsigned char buf[3];
 
 int main(void)
 {
-	buf[0] = recv; // 读recv三次,可能优化成只读第一次
+	buf[0] = recv;
 	buf[1] = recv;
 	buf[2] = recv;
-	send = ~buf[0]; // 写send三次,可能优化成只写最后一次
+	send = ~buf[0];
 	send = ~buf[1];
 	send = ~buf[2];
 
 	return 0;
 }
+
+// 见Makefile说明
