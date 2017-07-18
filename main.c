a41673ff5c58f12caddf0ec81db1c804f5da791e
u10 <u10@tcl.com>
2017-07-18 16:12:58 +0800

    ch22 pointer 打印命令行参数，busybox利用argv[0]程序名参数

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
 {
-	char *p ="abcd";
-	*p = 'A';
+	int i;
+	//for(i = 0; i < argc; i++)
+	for(i=0; argv[i] != NULL; i++)
+		printf("argv[%d]=%s\n", i, argv[i]);
 	return 0;
 }
+
+#if 0
+./a.out 1 2 3
+ln -s a.out printarg.link
+
+/bin/busybox: mv,cp,ls,ifconfig都是busybox的符号链接
+#endif
