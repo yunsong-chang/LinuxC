04773905efeff1fafbe0269eba9bc5dc9977e8f0
u10 <u10@tcl.com>
2017-07-18 16:46:40 +0800

    ch22 pointer const: from p_p_a to cp_cp_ca

--- main.c
+++ main.c
@@ -1,19 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-	int i;
-	//for(i = 0; i < argc; i++)
-	for(i=0; argv[i] != NULL; i++)
-		printf("argv[%d]=%s\n", i, argv[i]);
+	char *p_a = NULL;
+	const char *p_ca = NULL;
+	char * const cp_a = NULL;
+	const char * const cp_ca = NULL;
+
+	p_ca = p_a;
+	// p_a = p_ca;  		// warning: assignment discards qualifiers from pointer target type
+	// cp_a = p_a;			// error: assignment of read-only variable ‘cp_a’
+
+	char **p_p_a = NULL;
+	const char **p_p_ca = NULL;
+	char * const *p_cp_a = NULL;
+	const char * const *p_cp_ca = NULL;
+
+	char ** const cp_p_a = NULL;
+	const char * * const cp_p_ca = NULL;
+	char * const * const cp_cp_a = NULL;
+	const char * const *const cp_cp_ca = NULL;
+
+
+	// p_p_ca = p_p_a; 		// warning: assignment from incompatible pointer type
+	p_cp_a = p_p_a;			// 
+	// p_cp_ca = p_p_a;
+	p_cp_ca = p_p_ca;
+	
+
+// 000
+	p_p_a = 0;
+	*p_p_a = 0;
+	**p_p_a = 0;
+
+// 001
+	p_p_ca = 0;
+	*p_p_ca = 0;
+	// **p_p_ca = 0;
+
+// 010
+	p_cp_a = 0;
+	// *p_cp_a = 0;
+	**p_cp_a = 0;
+
+// 011
+	p_cp_ca = 0;
+	// *p_cp_ca = 0;
+	// **p_cp_ca = 0;
+
+// 100
+	// cp_p_a = 0;
+	*cp_p_a = 0;
+	**cp_p_a = 0;
+
+// 101
+	// cp_p_ca = 0;
+	*cp_p_ca = 0;
+	// **cp_p_ca = 0;
+
+// 110
+	// cp_cp_a = 0;
+	// *cp_cp_a = 0;
+	**cp_cp_a = 0;
+
+// 111
+	// cp_cp_ca = 0;
+	// *cp_cp_ca = 0;
+	// **cp_cp_ca = 0;
+
+
+
+	(void)p_a;
+	(void)p_ca;
+	(void)cp_a;
+	(void)cp_ca;
+
+	(void)p_p_a;
+	(void)p_p_ca;
+	(void)p_cp_a;
+	(void)p_cp_ca;
+
+	(void)cp_p_a;
+	(void)cp_p_ca;
+	(void)cp_cp_a;
+	(void)cp_cp_ca;
+
+	// (char *) p;
+	// p = p_a;
+
 	return 0;
 }
-
-#if 0
-./a.out 1 2 3
-ln -s a.out printarg.link
-
-/bin/busybox: mv,cp,ls,ifconfig都是busybox的符号链接
-#endif
