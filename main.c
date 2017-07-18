7ebb020719d4b545790f96389804123e343cf9d4
u10 <u10@tcl.com>
2017-07-18 15:31:27 +0800

    ch22 pointer rodata字符串，赋值给指针，segmentation fault

--- main.c
+++ main.c
@@ -1,24 +1,6 @@
-#include <stdio.h>
-
 int main()
 {
-	char c = 'a';
-	const char *p_C;
-	p_C = &c;
-
-	const char C = 'A';
-	char *p_c;
-	p_c = &C;
-//warning: assignment discards qualifiers from pointer target type
-
-	const char *p = "abcd";
-	const char str1[5] = "abcd";
-	char str2[5] = "abcd";
-
-	printf(p);  // warning: format not a string literal and no format arguments
-
-	printf(str1);
-	printf(str2); // warning: format not a string literal and no format arguments
-	printf("abcd");
+	char *p ="abcd";
+	*p = 'A';
 	return 0;
 }
