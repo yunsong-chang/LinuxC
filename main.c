34506383cf40e8d0c49f05b49de833e044647686
u10 <u10@tcl.com>
2017-07-21 10:03:08 +0800

    一切皆文件: 设备也是文件(网卡、打印机) 写文件：fprintf 读文件：fscanf

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main()
 {
 	int i, j;
 
-	printf("input the int value i=\n");
-	scanf("%d", &i);
-	printf("input the int value j=\n");
-	scanf("%d", &j);
-	printf("i+j=%d\n",i+j);
+	fprintf(stdout, "input the int value i=\n");
+	fscanf(stdin, "%d", &i);
+	fprintf(stdout, "input the int value j=\n");
+	fscanf(stdin, "%d", &j);
+	fprintf(stdout, "i+j=%d\n",i+j);
 	if (0 == j) {
 		fprintf(stderr, "error j = %d\n", j);
 	}
@@ -17,13 +17,7 @@ int main()
 }
 
 #if 0
-输入输出
-重定向
-
-./a.out 1>1.txt 2>2.txt <input.txt
-
-input.txt:
-8
-0
-
+	一切皆文件：设备也是文件（网卡、打印机）
+	写文件：fprintf
+	读文件：fscanf
 #endif
